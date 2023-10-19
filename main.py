2b12a7187a3d3d230463c0406096a8e2aa45dc1e
RedHeads-Enjoyer <dsc.cxc@mail.ru>
2023-10-19 11:59:12 +0300

    Added class Player

--- main.py
+++ main.py
@@ -10,3 +10,25 @@ class Question:
 
     def getQuestion(self):
         return self.question
+
+
+class Player:
+    def __init__(self, name, checkpoint):
+        self.name = name
+        self.score = 0
+        if (checkpoint <= 0 or checkpoint >= 6):
+            self.checkpoint = 3
+        else:
+            self.checkpoint = checkpoint
+
+    def addScore(self, number):
+        self.score += number
+
+    def getScore(self):
+        return self.score
+
+    def getName(self):
+        return self.name
+
+    def getCheckpoint(self):
+        return self.checkpoint
