5d32c5891cf0df7896e79b286eaa6b83ce593ce8
RedHeads-Enjoyer <dsc.cxc@mail.ru>
2023-10-19 11:58:36 +0300

    Added class Question

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
+class Question:
+    def __init__(self, question, answers):
+        if question[-1] != '?':
+            question += '?'
+        self.question = question
+        self.answers = answers
 
-# This is a sample Python script.
+    def getAnswers(self):
+        return self.answers
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def getQuestion(self):
+        return self.question
